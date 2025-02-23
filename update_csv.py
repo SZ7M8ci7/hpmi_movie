0efb162ea39a570c3db009136b742ee6460f3d01
SZ7M8ci7 <83046414+SZ7M8ci7@users.noreply.github.com>
2025-02-23 10:43:56 +0900

    コマンドの修正

--- update_csv.py
+++ update_csv.py
@@ -6,6 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+import chromedriver_binary
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
