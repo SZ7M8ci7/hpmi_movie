016c3aaa0df28b322bcef53b36ea00abd3ebc251
SZ7M8ci7 <83046414+SZ7M8ci7@users.noreply.github.com>
2025-02-23 10:49:21 +0900

    a

--- update_csv.py
+++ update_csv.py
@@ -68,7 +68,7 @@ def get_victory_count(url):
     options = Options()
     options.headless = True  # ヘッドレスモードで実行
 
-    options.add_argument('--user-data-dir=ttttmp')
+    options.add_argument('--headless')
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
     # URLにアクセス
