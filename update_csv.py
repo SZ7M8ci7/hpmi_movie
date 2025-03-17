006a8074cb2491a82cb138aef528e1e9776f508a
SZ7M8ci7 <83046414+SZ7M8ci7@users.noreply.github.com>
2025-03-17 20:11:32 +0900

    テンプレートマッチングに修正

--- update_csv.py
+++ update_csv.py
@@ -17,7 +17,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from PIL import Image, ImageDraw, ImageFont
 from PIL import Image, ImageOps
 import base64
-# import chromedriver_binary
+import chromedriver_binary
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
     'Referer': 'https://hypnosismic-movie.com',
